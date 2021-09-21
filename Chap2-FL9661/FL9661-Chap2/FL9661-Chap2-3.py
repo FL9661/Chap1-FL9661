1d57f5d0fe8d92a87cd54d1dc944294dbcd9f11b
FL9661 <FrancisLyness@hotmail.co.uk>
2021-09-21 07:23:46 +0100

    Added 2.3 operator note

--- Chap2-FL9661/FL9661-Chap2/FL9661-Chap2-3.py
+++ Chap2-FL9661/FL9661-Chap2/FL9661-Chap2-3.py
@@ -7,7 +7,7 @@ print ("print((5 * (0.52 + 100) / 26)) // 2)")
 print ("print (5 * 100.52 / 26) //2")
 print ("print (502.6 / 26) //2")
 print ("print (19.33 //2")
-print ("Print (19.33 / 2, then round up")
+print ("Print (19.33 / 2, then round down")
 print ("10", "\n)")
 
 #2.3 Operators
@@ -21,8 +21,9 @@ print ("print((16), (8.0), (8))", "\n")
 
 print ("Exercise 2")
 print ("print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))")
-print ("print((-0.5), (0.5), (0 // rounds up), (-0.5 // rounds up))")
+print ("print((-0.5), (0.5), (0 // + round down), (-0.5 // - rounds up))")
 print ("print((-0.5), (0.5), (0), (-1))", "\n")
+# + decimal values round down to positive whole number, - values round up to negative whole number
 
 print ("!!! Exercise 3 !!!")
 print ("print((2 % -4), (2 % 4), (2 ** 3 ** 2))")
