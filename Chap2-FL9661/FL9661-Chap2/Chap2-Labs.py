d69eb78c5c27baed2a476b2727bd074bbbafe1e4
Francis Peter Lyness <FrancisLyness@hotmail.co.uk>
2021-09-21 09:25:17 +0100

    upload Chap2-Labs.py file

---
+++ Chap2-FL9661/FL9661-Chap2/Chap2-Labs.py
@@ -0,0 +1,44 @@
+print ("Lab 2.4.1.7")
+# Step 1
+Var1 = ("John")
+Var2 = ("Mary")
+Var3 = ("Adam")
+
+# Step 2
+John = 3 
+Mary = 5
+Adam = 6 
+
+# Step 3
+print (John, Mary, Adam, sep=", ")
+
+# Step 4
+total_apples = John + Mary + Adam
+
+# Step 5
+print ("total_apples =", total_apples, "\n")
+
+
+print ("Lab 2.4.1.9")
+kilometers = 12.25
+miles = 7.38
+miles_to_kilometers = (miles * 1.61)
+kilometers_to_miles = (kilometers / 1.61)
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles", "\n")
+
+print ("Lab 2.4.1.10")
+x =  -1
+x = float(x)
+y = ((3 * x **3)-(2 * x **2)+(3 * x)-1)
+print("y =", y, "\n")
+
+print ("Lab 2.5.1.2")
+# this program computes the number of seconds in a given number of hours
+# Program has been written 20 Sept 2021
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+print("Hours: ", a) # Prints predefined variable of the number of hours
+print("Hours in Seconds: ", a * seconds) # multiplies the number of hours by the number of seconds of an hour
+#here we should also print "Goodbye", but a programmer didn't have time to write any code
+#this is the end of the program that computes the number of seconds in 2 hour
