2eec66734627cabffd033d383037e1aa55697300
FL9661 <FrancisLyness@hotmail.co.uk>
2021-09-22 20:36:02 +0100

    3.2

--- Chap3-FL9661/FL9661-Chap3-2.py
+++ Chap3-FL9661/FL9661-Chap3-2.py
@@ -78,5 +78,15 @@ print("The height of the pyramid:", height)
 #YouTube help used but I still did not understand
 
 ### 3.2.1.14 - building pyramid blocks  - HELP NEEDED HERE ###
+blocks = int(input("Enter the number of blocks: "))
+height = 0
+in_layer = 1
+while in_layer <= blocks: # while layer is less than or equal to blocks
+    height += 1 # hieght = height + 1
+    blocks -= in_layer # blocks = blocks - in layer
+    in_layer += 1 # in_layer = inlayer + 1
+print("The height of the pyramid:", height)
+print("The length of the pyramid:", in_layer)
+#YouTube help used but I still did not understand
+#height works out correct but the length is incorrect
 
-#still trying to work this out on the white board
