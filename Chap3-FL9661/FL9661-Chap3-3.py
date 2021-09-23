df5ba08fec60e765bbbdcd42aeaabfec4a5f565d
FL9661 <FrancisLyness@hotmail.co.uk>
2021-09-23 23:03:19 +0100

    -

--- Chap3-FL9661/FL9661-Chap3-3.py
+++ Chap3-FL9661/FL9661-Chap3-3.py
@@ -22,15 +22,14 @@ print ('number of list entries is: ', len(hat_numbers))
 # Lab 3.4.1.13: the Beatles #
 
 # step 1 create an empty list named beatles;
-
 beatles = [] # name of list = [] empty element container
-print('Step 1:', beatles)
+print('Step 1:', beatles) # Outputs list elemetns to console 
 
 # step 2 - Add first memebr elements
 beatles.append('John Lennon') #0
 beatles.append('Paul McCartney') #1
 beatles.append('George Harrison') #2
-print('Step 2:', beatles)
+print('Step 2:', beatles) # Outputs list elemetns to console 
 
 # step 3 - input new memebr elements
 a = input ('enter Pete Best: ')
@@ -49,6 +48,21 @@ del beatles [4] # Del George Harrison
 del beatles [3] # Del Stu Sutcliffe
 
 print('step 4:', beatles)
+print("The Fab", len(beatles)) # testing list legth
 
-# testing list legth
-print("The Fab", len(beatles))
+# Lab 3.6.1.9 - copying uniques numbers to a new list
+
+Forename = input('Please enter your Forename: ') # users firstname
+Surname = input('Please enter your Forename: ') # users surname
+mylist = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]#current list
+newlist = [] #new blank list
+
+print('Hello', Forename, Surname, 'I have taken your old list that contained')
+print('these values ', mylist, '.', '\n', '\n', sep='' )
+
+for i in mylist: #for numbers within mylist
+        if i not in newlist: #if i is unique not in newlist
+            newlist.append (i) # add i to new list
+
+print('I have made a new list that has', (len(newlist)), 'unique instances. These instances are:')
+print (newlist)
--- Submisssions/test.py
+++ Submisssions/test.py
@@ -1,13 +1,16 @@
-newbeatles = ('ss')
-for elements in newbeatles:
-   
-    while True:
-        userinput1 = input('Which band member had intials that where SS? ')
-        if  userinput1 != newbeatles:
-            print ('try again:', '\n')
-        elif userinput1 == newbeatles:
-            print ('correct')
-            break
-        else:
-             print ('close') 
-             end()
+# Lab 3.6.1.9 - copying uniques numbers to a new list # 
+
+Forename = input('Please enter your Forename: ') # users firstname
+Surname = input('Please enter your Forename: ') # users surname
+mylist = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]#current list
+newlist = [] #new blank list
+
+print('Hello', Forename, Surname, 'I have taken your old list that contained')
+print('these values ', mylist, '.', '\n', '\n', sep='' )
+
+for i in mylist: #for numbers within mylist
+        if i not in newlist: #if i is unique not in newlist
+            newlist.append (i) # add i to new list
+
+print('I have made a new list that has', (len(newlist)), 'unique instances. These instances are:')
+print (newlist)
