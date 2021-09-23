fab68947b6fe8ca0bf4cbbbb3fe375f454d08d03
Francis Peter Lyness <FrancisLyness@hotmail.co.uk>
2021-09-23 09:30:22 +0100

    -

--- Submisssions/test.py
+++ Submisssions/test.py
@@ -1,12 +1,12 @@
-### 3.2.1.14 - building pyramid blocks  - HELP NEEDED HERE ###
-blocks = int(input("Enter the number of blocks: "))
-height = 0
-in_layer = 1
-while in_layer <= blocks: # while layer is less than or equal to blocks
-    height += 1 # hieght = height + 1
-    blocks -= in_layer # blocks = blocks - in layer
-    in_layer += 1 # in_layer = inlayer + 1
-print("The height of the pyramid:", height)
-print("The length of the pyramid:", in_layer)
-#YouTube help used but I still did not understand
-#height works out correct but the length is incorrect
+### 3.2.1.14 - building pyramid blocks  ###
+
+blocks = int(input('Enter the number of blocks: '))
+
+height = 0 # Light Blub var
+in_row = 1 # Light Blub var, initial row is never below 1 but blocks can be zero
+while in_row <= blocks: # Light Blub var = is the row number less or equal to blocks
+    height += 1 # is less than blocks - add 1 to height
+    blocks -= in_row # now reduce blocks by initial row value
+    in_row +=1 # increase in_row value for next loop 
+print('The height of the pyramid:', height) # prints hieght
+# to print length insert "print ('The height of the pyramid:', (in_row - 1))""
