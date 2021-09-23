eb6d15759fa6ab67ebabd5e2ef07a5424c75ee09
FL9661 <FrancisLyness@hotmail.co.uk>
2021-09-23 18:12:38 +0100

    -

--- Chap3-FL9661/FL9661-Chap3-3.py
+++ Chap3-FL9661/FL9661-Chap3-3.py
@@ -1,3 +1,24 @@
+# F Lyness
+# Date 23 Sept 2021
+
+# Lab 3.4.1.6: the basics of lists #
+# Task: use a list to print a series of numbers 
+
+hat_numbers = [1,2,3,4,5]
+#list containing each number value known as an element
+
+print ('There once was a hat. The hat contained no rabbit, but a list of five numbers: ', hat_numbers [0], hat_numbers [1], hat_numbers [2], hat_numbers [3], 'and', hat_numbers [4])
+# prints each element value using its index numeber
+
+hat_numbers [3] = input('Please assign new number value: ')
+# step 1 prompt the user to replace the middle element value
+
+del hat_numbers [-1]
+# removes the last element from the list (Step 2)
+
+print ('number of list entries is: ', len(hat_numbers))
+#outputs to the console number of elements within the list 
+
 # Lab 3.4.1.13: the Beatles #
 
 # step 1 create an empty list named beatles;
--- Submisssions/test.py
+++ Submisssions/test.py
@@ -1,10 +1,13 @@
-### 3.2.1.14 - building pyramid blocks  ###
-blocks = int(input('Enter the number of blocks: '))
-height = 0 # Light Blub var
-in_row = 1 # Light Blub var, initial row is never below 1 but blocks can be zero
-while in_row <= blocks: # Light Blub var = is the row number less or equal to blocks
-    height += 1 # is less than blocks - add 1 to height
-    blocks -= in_row # now reduce blocks by initial row value
-    in_row +=1 # increase in_row value for next loop 
-print('The height of the pyramid:', height) # prints hieght
-# to print length insert "print ('The height of the pyramid:', (in_row - 1))""
+newbeatles = ('ss')
+for elements in newbeatles:
+   
+    while True:
+        userinput1 = input('Which band member had intials that where SS? ')
+        if  userinput1 != newbeatles:
+            print ('try again:', '\n')
+        elif userinput1 == newbeatles:
+            print ('correct')
+            break
+        else:
+             print ('close') 
+             end()
