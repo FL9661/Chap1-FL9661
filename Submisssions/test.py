05e41b462be66ecee96bb089a906c8eecaaec32a
FL9661 <FrancisLyness@hotmail.co.uk>
2021-09-23 12:35:44 +0100

    -

--- Submisssions/test.py
+++ Submisssions/test.py
@@ -1,12 +1,18 @@
-### 3.2.1.14 - building pyramid blocks  - HELP NEEDED HERE ###
-blocks = int(input("Enter the number of blocks: "))
-height = 0
-in_layer = 1
-while in_layer <= blocks: # while layer is less than or equal to blocks
-    height += 1 # hieght = height + 1
-    blocks -= in_layer # blocks = blocks - in layer
-    in_layer += 1 # in_layer = inlayer + 1
-print("The height of the pyramid:", height)
-print("The length of the pyramid:", in_layer)
-#YouTube help used but I still did not understand
-#height works out correct but the length is incorrect
+# Lab 3.4.1.6: the basics of lists #
+# F Lyness
+# Task: use a list to print a series of numbers 
+
+hat_numbers = [1,2,3,4,5]
+#list containing each number value known as an element
+
+print ('There once was a hat. The hat contained no rabbit, but a list of five numbers: ', hat_numbers [0], hat_numbers [1], hat_numbers [2], hat_numbers [3], 'and', hat_numbers [4])
+# prints each element value using its index numeber
+
+hat_numbers [3] = input('Please assign new number value: ')
+# step 1 prompt the user to replace the middle element value
+
+del hat_numbers [-1]
+# removes the last element from the list (Step 2)
+
+print ('number of list entries is: ', len(hat_numbers))
+#outputs to the console number of elements within the list 
